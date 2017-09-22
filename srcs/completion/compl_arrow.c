3c714b88021d9c119570c1d07225bdaa51280301
amazurie <amazu@live.fr>
2017-09-22 16:00:06 +0200

    display redone

--- srcs/completion/compl_arrow.c
+++ srcs/completion/compl_arrow.c
@@ -1,76 +1,10 @@
 #include "shell.h"
 
-static void	gest_left(t_compl *compl, t_cmd *cmd)
-{
-	struct winsize	w;
-	int				nbrpercol;
-	int				nbrperline;
-	int				i;
-
-	ioctl(0, TIOCGWINSZ, &w);
-	i = maxrow_line(ft_strlen(cmd->str) + ft_strlen(cmd->prompt)
-			+ compl->maxlen, w.ws_col);
-	nbrperline = nbr_perline(compl->maxlen, w.ws_col);
-	nbrpercol = nbr_percol(compl->nbrargs, nbrperline, w.ws_row, i);
-	i = nbrpercol;
-	if (nbrpercol * nbrperline < compl->nbrargs)
-	{
-		i = 1;
-		while (i * nbrperline < compl->nbrargs - nbrpercol * nbrperline)
-			i++;
-	}
-	if (nbrperline == 1)
-		compl->curr = (compl->curr > 0) ? compl->curr - 1 : compl->nbrargs - 1;
-	else if (compl->curr - i >= 0)
-		compl->curr -= i;
-	else
-	{
-		while (compl->curr + i < compl->nbrargs)
-			compl->curr += i;
-		if (compl->curr + i == compl->nbrargs)
-			compl->curr = compl->nbrargs - 1;
-		else if (compl->curr - 1 < compl->nbrargs - i)
-			compl->curr = compl->nbrargs - i;
-		else
-			compl->curr -= 1;
-	}
-}
-
-static void	gest_right(t_compl *compl, t_cmd *cmd)
-{
-	struct winsize	w;
-	int				nbrpercol;
-	int				nbrperline;
-	int				i;
-
-	ioctl(0, TIOCGWINSZ, &w);
-	i = maxrow_line(ft_strlen(cmd->str) + ft_strlen(cmd->prompt)
-			+ compl->maxlen, w.ws_col);
-	nbrperline = nbr_perline(compl->maxlen, w.ws_col);
-	nbrpercol = nbr_percol(compl->nbrargs, nbrperline, w.ws_row, i);
-	i = nbrpercol;
-	if (nbrpercol * nbrperline < compl->nbrargs)
-	{
-		i = 1;
-		while (i * nbrperline < compl->nbrargs - nbrpercol * nbrperline)
-			i++;
-	}
-	if (nbrperline == 1)
-		compl->curr = (compl->curr < compl->nbrargs - 1) ? compl->curr + 1 : 0;
-	else if (compl->curr + i < compl->nbrargs)
-		compl->curr += i;
-	else
-	{
-		while (compl->curr - i >= 0)
-			compl->curr -= i;
-		if (++compl->curr >= i)
-			compl->curr = 0;
-	}
-}
-
 int			compl_arrow(t_compl *compl, t_cmd *cmd, char *buf)
 {
-	if (buf[2] == 68)
+	if (compl || cmd || buf)
+		;
+/*	if (buf[2] == 68)
 		gest_left(compl, cmd);
 	else if (buf[2] == 67)
 		gest_right(compl, cmd);
@@ -80,7 +14,5 @@ int			compl_arrow(t_compl *compl, t_cmd *cmd, char *buf)
 		compl->curr = (compl->curr < compl->nbrargs - 1) ? compl->curr + 1 : 0;
 	else
 		return (0);
-	if (compl || cmd || buf)
-		;
-	return (1);
+*/	return (1);
 }
--- srcs/completion/compl_keys.c
+++ srcs/completion/compl_keys.c
@@ -12,57 +12,6 @@
 
 #include "shell.h"
 
-static int	is_onscreen(t_compl *compl, t_cmd *cmd, struct winsize w, int hided)
-{
-	t_coargs	*args;
-	int			nbrpercol;
-	int			nbrperline;
-	int			i;
-
-	args = &compl->args;
-	i = maxrow_line(ft_strlen(cmd->str) + ft_strlen(cmd->prompt)
-			+ compl->maxlen, w.ws_col);
-	nbrperline = nbr_perline(compl->maxlen, w.ws_col);
-	nbrpercol = nbr_percol(compl->nbrargs, nbrperline, w.ws_row, i);
-	i = -1;
-	while (++i < nbrpercol)
-	{
-		if (compl->curr >= compl->toskip + i * (hided + nbrpercol)
-		&& compl->curr < compl->toskip + i * (hided + nbrpercol) + nbrpercol - i)
-			return (1);
-	}
-	return (0);
-}
-
-static void	get_onscreen(t_compl *compl, t_cmd *cmd)
-{
-	struct winsize	w;
-	int				nbrpercol;
-	int				nbrperline;
-	int				i;
-	int				j;
-
-	ioctl(0, TIOCGWINSZ, &w);
-	i = maxrow_line(ft_strlen(cmd->str) + ft_strlen(cmd->prompt)
-			+ compl->maxlen, w.ws_col);
-	nbrperline = nbr_perline(compl->maxlen, w.ws_col);
-	nbrpercol = nbr_percol(compl->nbrargs, nbrperline, w.ws_row, i);
-	if (nbrpercol * nbrperline >= compl->nbrargs)
-		return ;
-	i = 1;
-	while (i * nbrperline < compl->nbrargs - nbrpercol * nbrperline)
-		i++;
-	j = compl->toskip;
-	while (compl->toskip <= i && !is_onscreen(compl, cmd, w, i))
-		compl->toskip++;
-	if (j > i)
-	{
-		compl->toskip = j - 1;
-		while (compl->toskip > 0 && !is_onscreen(compl, cmd, w, i))
-			compl->toskip--;
-	}
-}
-
 static void	add_argtoline(t_compl *compl, t_cmd *cmd)
 {
 	t_coargs	*args;
@@ -81,11 +30,14 @@ int		compl_keys(t_compl *compl, t_cmd *cmd, char **buf)
 	if ((*buf)[0] == 27 && !(*buf)[1])
 		return (-1);
 	else if ((*buf)[0] == 27 && compl_arrow(compl, cmd, *buf))
+	{
+//		get_onscreen(compl, cmd);
 		display_args(compl, cmd);
+	}
 	else if ((*buf)[0] == 9 && !(*buf)[1])
 	{
 		compl->curr = (compl->curr < compl->nbrargs - 1) ? compl->curr + 1 : 0;
-		get_onscreen(compl, cmd);
+//		get_onscreen(compl, cmd);
 		display_args(compl, cmd);
 	}
 	else if ((*buf)[0] == 10 && !(*buf)[1])
