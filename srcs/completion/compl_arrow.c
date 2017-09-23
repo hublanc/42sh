788be3775f6b3a1ca070299c4a784e19e833ad4c
amazurie <amazu@live.fr>
2017-09-23 10:52:58 +0200

    compltation arrow + end, home key handle properly

--- srcs/completion/compl_arrow.c
+++ srcs/completion/compl_arrow.c
@@ -1,18 +1,77 @@
 #include "shell.h"
 
+static int	compl_endhome(t_compl *compl, t_cmd *cmd, char *buf)
+{
+	int	*size;
+
+	size = get_size(compl, cmd);
+	if ((buf[0] == 27 && buf[1] == 91 && (buf[2] == 72 || buf[2] == 70))
+			|| ((buf[0] == 1 || buf[0] == 5) && !buf[1]))
+	{
+		size[6] = 0;
+		if (buf[2] == 72 || buf[0] == 1)
+		{
+			while (compl->curr < size[4] * size[6]
+					|| compl->curr > size[4] * size[6] + size[4])
+				size[6]++;
+			compl->curr = size[4] * size[6];
+			free(size);
+			return (1);
+		}
+		while (compl->curr < size[4] * size[6]
+				|| compl->curr > size[4] * size[6] + size[4])
+			size[6]++;
+		compl->curr = size[4] * size[6] + --size[4];
+		free(size);
+		return (1);
+	}
+	free(size);
+	return (0);
+}
+
+static void	hand_rlcompl(t_compl *compl, t_cmd *cmd, char *buf)
+{
+	int	*size;
+
+	if ((buf[0] == 27 && buf[1] == 91 && buf[2] == 67 && !buf[3])
+			|| (buf[0] == 2 && !buf[1]))
+	{
+		size = get_size(compl, cmd);
+		if (compl->curr + size[4] > compl->nbrargs - 1 && ++compl->curr >= -1)
+			while (compl->curr - size[4] >= 0)
+				compl->curr -= size[4];
+		else
+			compl->curr += size[4];
+		free(size);
+	}
+	else
+	{
+		size = get_size(compl, cmd);
+		if (compl->curr - size[4] < 0 && --compl->curr >= -1)
+			while (compl->curr + size[4] <= compl->nbrargs - 1)
+				compl->curr += size[4];
+		else
+			compl->curr -= size[4];
+		free(size);
+	}
+}
+
 int			compl_arrow(t_compl *compl, t_cmd *cmd, char *buf)
 {
-	if (compl || cmd || buf)
-		;
-/*	if (buf[2] == 68)
-		gest_left(compl, cmd);
-	else if (buf[2] == 67)
-		gest_right(compl, cmd);
-	else if (buf[2] == 65)
-		compl->curr = (compl->curr > 0) ? compl->curr - 1 : compl->nbrargs - 1;
-	else if (buf[2] == 66)
-		compl->curr = (compl->curr < compl->nbrargs - 1) ? compl->curr + 1 : 0;
+	if ((buf[0] == 27 && buf[1] == 91 && ((buf[2] == 65 && !buf[3])
+					|| (buf[2] == 53 && buf[3] == 126)))
+			|| (buf[0] == 16 && !buf[1]))
+		compl->curr = (compl->curr - 1 < 0) ? compl->nbrargs - 1 : compl->curr - 1;
+	else if ((buf[0] == 27 && buf[1] == 91 && ((buf[2] == 66 && !buf[3])
+					|| (buf[2] == 54 && buf[3] == 126)))
+			|| (buf[0] == 14 && !buf[1]))
+		compl->curr = (compl->curr + 1 > compl->nbrargs - 1) ? 0 : compl->curr + 1;
+	else if (((buf[0] == 27 && buf[1] == 91 && buf[2] == 67 && !buf[3])
+				|| (buf[0] == 2 && !buf[1])) ||
+			((buf[0] == 27 && buf[1] == 91 && buf[2] == 68 && !buf[3])
+			 || (buf[0] == 6 && !buf[1])))
+		hand_rlcompl(compl, cmd, buf);
 	else
-		return (0);
-*/	return (1);
+		return (compl_endhome(compl, cmd, buf));
+	return (1);
 }
