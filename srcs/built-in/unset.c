d205a7093cacf50c5d877f735aa08bc73d0f7462
amazurie <amazu@live.fr>
2017-10-06 10:23:01 +0200

    export + fix '=' on cmd

--- srcs/built-in/unset.c
+++ srcs/built-in/unset.c
@@ -17,38 +17,40 @@ static int	argunset(char *name, char ***env)
 	char	**tab;
 	int		i;
 
-	if (!get_loc(name))
+	if (!get_loc(name) && env && *env)
 	{
 		i = 0;
 		while ((*env)[i] && ft_strncmp((*env)[i], name,
 					ft_strlen_chr((*env)[i], '=') - 1))
 			i++;
 		if (!(*env)[i] || ft_strcmp((*env)[i], name))
-				return (0);
+				return (1);
 		else
 		{
 			if(!(tab = (char **)ft_memalloc(sizeof (char *) * 3)))
-					return (0);
+					return (1);
 			tab[0] = "unsetenv";
 			tab[1] = name;
 			tab[2] = NULL;
-			ft_unsetenv(tab, env);
+			i = ft_unsetenv(tab, env);
 			free(tab);
+			return (i);
 		}
 	}
-	return (1);
+	return (0);
 }
 
 int			unset(char **tab, char ***env)
 {
 	int	i;
-	int	j;
+	int	status;
 
 	if (tab[0] && !tab[1])
-		ft_putstr_fd("unset: not enough arguments", 2);
+		return (0);
 	i = 0;
-	j = 1;
+	status = 0;
 	while (tab[++i])
-		j = argunset(tab[i], env);
-	return (j);
+		if (argunset(tab[i], env))
+				status = 1;
+	return (status);
 }
