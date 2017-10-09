8a8cec210a146b2e37beb8b1d58070f068720cbc
Arnaud MAZURIER <amazurie@e1r11p19.42.fr>
2017-10-09 17:07:50 +0200

    resolution path

--- includes/cd.h
+++ includes/cd.h
@@ -6,7 +6,7 @@
 /*   By: amazurie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 13:59:40 by amazurie          #+#    #+#             */
-/*   Updated: 2017/10/09 14:14:47 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/10/09 16:39:50 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 int			cd(char **path, char ***env);
 int			change_envpwd(char *tmp, char ***env);
 char		*get_elem(char ***env, char *elem);
-char		*check_path(char *path, char ***env);
+char		*check_path(char *path, char ***env, char opt);
+void		check_dotdot(char **tmp, char **path);
