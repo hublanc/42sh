0ecd40a581ae5e64ee19f8c61608c2c1eed824b2
Lucas BOPP <lbopp@student.42.fr>
2017-10-10 15:41:12 +0200

    Prompt = Delete all .o

--- objs/terminal.d
+++
@@ -1,5 +0,0 @@
-objs/terminal.o: srcs/terminal.c includes/shell.h \
-  includes/../libft/includes/libft.h includes/exec.h \
-  includes/line_editor.h includes/lexer.h includes/redirection.h \
-  includes/tools.h includes/prompt_statement.h includes/completion.h \
-  includes/local.h
