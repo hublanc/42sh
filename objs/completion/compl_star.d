75523abf2afde3b50a8e71f6078f56dc6f4f7ab7
Lucas BOPP <lbopp@student.42.fr>
2017-10-11 16:49:32 +0200

    Removing obj

--- objs/completion/compl_star.d
+++
@@ -1,5 +0,0 @@
-objs/completion/compl_star.o: srcs/completion/compl_star.c \
-  includes/shell.h includes/../libft/includes/libft.h includes/exec.h \
-  includes/line_editor.h includes/lexer.h includes/redirection.h \
-  includes/tools.h includes/prompt_statement.h includes/completion.h \
-  includes/local.h
--- objs/completion/print_complline.d
+++
@@ -1,5 +0,0 @@
-objs/completion/print_complline.o: srcs/completion/print_complline.c \
-  includes/shell.h includes/../libft/includes/libft.h includes/exec.h \
-  includes/line_editor.h includes/lexer.h includes/redirection.h \
-  includes/tools.h includes/prompt_statement.h includes/completion.h \
-  includes/local.h
