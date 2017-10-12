8835738179ac97d57f35748cb2243f91f2a2475d
mameyer <mameyer@student.42.fr>
2017-10-12 15:36:33 +0200

    Update

--- srcs/line_editor/enter_handler.c
+++ srcs/line_editor/enter_handler.c
@@ -6,7 +6,7 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/11 14:30:26 by hublanc           #+#    #+#             */
-/*   Updated: 2017/10/06 12:06:04 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/10/12 15:35:03 by mameyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void		enter_handler(t_cmd *cmd, t_control **history, char ***env)
 {
 	char	c;
 
+	set_selected_null(history);
 	ft_putchar('\n');
 	if (!cmd->str)
 		return (choose_prompt(cmd));
