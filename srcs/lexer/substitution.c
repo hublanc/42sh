4d768f2e1552c69015dc05b1e75ccd71c5757467
amazurie <amazu@live.fr>
2017-10-12 10:28:23 +0200

    split func

--- srcs/lexer/substitution.c
+++ srcs/lexer/substitution.c
@@ -6,7 +6,7 @@
 /*   By: amazurie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/10 15:49:56 by amazurie          #+#    #+#             */
-/*   Updated: 2017/10/10 16:52:38 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/10/12 10:24:27 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,32 @@ char	*get_elem(char ***env, char *elem)
 	return ((*env)[i] + ft_strlen(elem));
 }
 
-void	substitution(char **cmmd, char ***env)
+static void	substitute(char **cmmd, char ***env, int i, int j)
 {
 	char	*tmp;
 	char	*tmp2;
+
+	tmp2 = ft_strndup(*cmmd + i + 1, j - i - 1);
+	//futur ajout local
+	if (tmp2 && tmp2[0] && (tmp = get_elem(env, tmp2)) && *(++tmp))
+	{
+		free(tmp2);
+		tmp2 = ft_strndup(*cmmd, i);
+		tmp = ft_strjoin(tmp2, tmp);
+		(tmp2) ? free(tmp2) : 0;
+		tmp2 = ft_strjoin(tmp, *cmmd + j);
+		(tmp) ? free(tmp) : 0;
+		(tmp2) ? free(*cmmd) : 0;
+		(tmp2) ? *cmmd = ft_strdup(tmp2) : 0;
+	}
+	else
+		while (j-- > i)
+			ssupprchr(cmmd, i);
+	(tmp2) ? free(tmp2) : 0;
+}
+
+void		substitution(char **cmmd, char ***env)
+{
 	int		i;
 	int		j;
 
@@ -52,27 +74,11 @@ void	substitution(char **cmmd, char ***env)
 			while ((*cmmd)[i] && (*cmmd)[++i] != '\'');
 		j = i + 1;
 		if ((*cmmd)[i] == '$')
-			while ((*cmmd)[j] && (*cmmd)[j] != 32 && (*cmmd)[j] != 9 && (*cmmd)[j] != '\n' && (*cmmd)[j] != '"' && (*cmmd)[j] != '$')
+			while ((*cmmd)[j] && (*cmmd)[j] != 32 && (*cmmd)[j] != 9
+					&& (*cmmd)[j] != '\n' && (*cmmd)[j] != '"'
+					&& (*cmmd)[j] != '$')
 				j++;
 		if (j > i + 1)
-		{
-			tmp2 = ft_strndup(*cmmd + i + 1, j - i - 1);
-			//futur ajout local
-			if (tmp2 && tmp2[0] && (tmp = get_elem(env, tmp2)) && *(++tmp))
-			{
-				free(tmp2);
-				tmp2 = ft_strndup(*cmmd, i);
-				tmp = ft_strjoin(tmp2, tmp);
-				(tmp2) ? free(tmp2) : 0;
-				tmp2 = ft_strjoin(tmp, *cmmd + j);
-				(tmp) ? free(tmp) : 0;
-				(tmp2) ? free(*cmmd) : 0;
-				(tmp2) ? *cmmd = ft_strdup(tmp2) : 0;
-			}
-			else
-				while (j-- > i)
-					ssupprchr(cmmd, i);
-			(tmp2) ? free(tmp2) : 0;
-		}
+			substitute(cmmd, env, i, j);
 	}
 }
