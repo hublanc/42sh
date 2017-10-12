31c25e2258a017366857686238b784fb59a9e911
Hugo BLANC <hublanc@e1r11p19.42.fr>
2017-10-12 12:26:23 +0200

    norme header file + fix cmdsplit

--- includes/completion.h
+++ includes/completion.h
@@ -6,7 +6,7 @@
 /*   By: amazurie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 09:50:30 by amazurie          #+#    #+#             */
-/*   Updated: 2017/09/28 14:48:41 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/10/12 12:15:55 by hublanc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,51 +56,53 @@ void				list_compl(t_compl *compl, t_cmd *cmd, char ***env);
 ** get_binaries.c
 */
 void				get_args(t_compl *compl, char **paths);
-int					get_files(t_compl *compl, DIR *dirp, t_coargs **ar, int *fd);
+int					get_files(t_compl *compl, DIR *dirp,
+					t_coargs **ar, int *fd);
 
 /*
 ** display_args.c
 */
-void		display_args(t_compl *compl, t_cmd *cmd);
-int			*get_size(t_compl *compl, t_cmd *cmd);
+void				display_args(t_compl *compl, t_cmd *cmd);
+int					*get_size(t_compl *compl, t_cmd *cmd);
 
 /*
 ** compl_keys.c
 */
-int			compl_keys(t_compl *compl, t_cmd *com, char **buf);
+int					compl_keys(t_compl *compl, t_cmd *com, char **buf);
 
 /*
 ** compl_arrow.c
 */
-int			compl_arrow(t_compl *compl, t_cmd *cmd, char *buf);
+int					compl_arrow(t_compl *compl, t_cmd *cmd, char *buf);
 
 /*
 ** compl_star.c
 */
-int			compl_star(t_compl *compl, t_cmd *cmd);
+int					compl_star(t_compl *compl, t_cmd *cmd);
 
 /*
 ** compl_color.c
 */
-void		compl_addcolor(t_coargs **ar, char *path);
+void				compl_addcolor(t_coargs **ar, char *path);
 
 /*
 ** print_complline.c
 */
-void		print_complline(t_compl *compl, t_cmd *cmd, int *size, char **buff);
-void		add_argtoline(t_compl *compl, t_cmd *cmd);
+void				print_complline(t_compl *compl, t_cmd *cmd,
+					int *size, char **buff);
+void				add_argtoline(t_compl *compl, t_cmd *cmd);
 
 /*
 ** compl_buff.c
 */
-size_t		print_buff(char **buff);
-size_t		buffcat(char **buff, char *s);
-size_t		buffncat(char **buff, char *s, size_t n);
+size_t				print_buff(char **buff);
+size_t				buffcat(char **buff, char *s);
+size_t				buffncat(char **buff, char *s, size_t n);
 
 /*
 ** compl_free.c
 */
-void		free_tabl(char **tab);
-void		compl_free(t_compl *compl);
+void				free_tabl(char **tab);
+void				compl_free(t_compl *compl);
 
 #endif
