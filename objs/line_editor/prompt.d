06244048122cedb513f7bf865bc5a09dfe1e40c2
Lucas BOPP <lbopp@student.42.fr>
2017-10-16 15:05:41 +0200

    READ with signal

--- objs/line_editor/prompt.d
+++
@@ -1,6 +0,0 @@
-objs/line_editor/prompt.o: srcs/line_editor/prompt.c includes/shell.h \
-  includes/../libft/includes/libft.h includes/../libft/includes/libdll.h \
-  includes/../libft/includes/ft_getopt.h includes/exec.h \
-  includes/line_editor.h includes/lexer.h includes/redirection.h \
-  includes/tools.h includes/prompt_statement.h includes/completion.h \
-  includes/local.h
