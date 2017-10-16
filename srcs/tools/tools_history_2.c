1330eb335f96722337e6135f184302c9532b55ce
mameyer <mameyer@student.42.fr>
2017-10-16 16:57:53 +0200

    Update - Multiline history done

--- srcs/tools/tools_history_2.c
+++ srcs/tools/tools_history_2.c
@@ -6,7 +6,7 @@
 /*   By: mameyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 15:57:56 by mameyer           #+#    #+#             */
-/*   Updated: 2017/10/13 16:05:22 by mameyer          ###   ########.fr       */
+/*   Updated: 2017/10/16 16:12:27 by mameyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,28 @@
 
 void		add_hist_or_not(t_control **history, char *str)
 {
+	int		i;
+	char	*cpy;
+
+	i = 0;
+	cpy = NULL;
+	cpy = ft_strdup(str);
+	while (ft_strchr(cpy, '\n'))
+	{
+		i = ft_strchr(cpy, '\n') - cpy;
+		ft_putendl(cpy);
+		cpy[i] = ' ';
+	}
 	if (*history && (*history)->length == 0)
-		(*history) = dll_add_new_elem_frnt(*history, str);
+		(*history) = dll_add_new_elem_frnt(*history, cpy);
 	else if (*history && (*history)->begin && (*history)->begin->name
-		&& ft_strcmp((*history)->begin->name, str) != 0)
-		(*history) = dll_add_new_elem_frnt(*history, str);
+		&& ft_strcmp((*history)->begin->name, cpy) != 0)
+		(*history) = dll_add_new_elem_frnt(*history, cpy);
 	else if (*history && (*history)->begin && (*history)->begin->name
-		&& ft_strcmp((*history)->begin->name, str) == 0)
+		&& ft_strcmp((*history)->begin->name, cpy) == 0)
 		return ;
 	else
-		(*history) = dll_add_new_elem_frnt(*history, str);
+		(*history) = dll_add_new_elem_frnt(*history, cpy);
 }
 
 void		change_cmd(char *new, t_cmd *cmd)
