9884837b79567ee413d9378787d31a97aae18509
mameyer <mameyer@student.42.fr>
2017-10-18 20:00:55 +0200

    Up

--- libft/ft_memalloc.c
+++ libft/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 11:17:36 by hublanc           #+#    #+#             */
-/*   Updated: 2017/01/16 18:29:12 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/10/18 19:50:44 by mameyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*zone;
 
+//	return (NULL);
 	if (!(zone = malloc(size)))
 		return (0);
 	ft_memset(zone, 0, size);
