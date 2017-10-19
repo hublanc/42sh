d51b369b125d5e903451fbcee023ed4e123a6b9f
amazurie <amazu@live.fr>
2017-10-19 11:58:11 +0200

    termobtain

--- srcs/terminal.c
+++ srcs/terminal.c
@@ -6,26 +6,76 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 11:40:20 by hublanc           #+#    #+#             */
-/*   Updated: 2017/10/11 21:24:39 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/10/19 11:57:38 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+int			read_terminfo(char *tmp)
+{
+	struct dirent	*dirc;
+	DIR				*dirp2;
+
+	if (!(dirp2 = opendir(tmp)))
+		return (0);
+	while ((dirc = readdir(dirp2)))
+	{
+		if (tgetent(NULL, dirc->d_name) > 0 && tgetstr("do", NULL)
+				&& tgetstr("nd", NULL) && tgetstr("mr", NULL)
+				&& tgetstr("me", NULL) && tgetstr("up", NULL)
+				&& tgetstr("cd", NULL) && tgetstr("cr", NULL)
+				&& tgetstr("sc", NULL) && tgetstr("rc", NULL)
+				&& tgetstr("ho", NULL) && tgetstr("le", NULL)
+				&& tgetstr("cl", NULL))
+		{
+			closedir(dirp2);
+			return (1);
+		}
+	}
+	closedir(dirp2);
+	return (0);
+}
+
+int			get_terminfo(void)
+{			
+	struct dirent	*dirc;
+	DIR				*dirp;
+	char			*tmp;
+
+	if (!(dirp = opendir("/usr/share/terminfo")))
+		return (0);
+	while ((dirc = readdir(dirp)))
+	{
+		tmp = ft_strjoin("/usr/share/terminfo/", dirc->d_name);
+		if (!tmp)
+		{
+			(tmp) ? free(tmp) : 0;
+			closedir(dirp);
+			return (0);
+		}
+		if (read_terminfo(tmp) == 1)
+		{
+			closedir(dirp);
+			free(tmp);
+			return (1);
+		}
+		free(tmp);
+	}
+	closedir(dirp);
+	return (0);
+}
+
 void		set_terminal(void)
 {
-	char			*name_term;
 	struct termios	term;
+	char			*name_term;
 
 	name_term = getenv("TERM");
-	if (!name_term || !*name_term)
+	if (((!name_term || !*name_term) || tgetent(NULL, name_term) == -1)
+			&& !get_terminfo())
 	{
-		ft_putstr_fd("TERM not found\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	if (tgetent(NULL, name_term) == -1)
-	{
-		ft_putstr_fd("TERM invalid\n", 2);
+		ft_putstr_fd("Could not load TERM, exiting...\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	tcgetattr(0, &term);
