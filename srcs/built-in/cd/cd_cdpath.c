efdff30078d32c607b792de892d51008c6dbaae0
Lucas BOPP <lbopp@student.42.fr>
2017-10-30 17:50:01 +0100

    Cd: Norme

---
+++ srcs/built-in/cd/cd_cdpath.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd_cdpath.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/30 16:52:05 by lbopp             #+#    #+#             */
+/*   Updated: 2017/10/30 17:37:34 by lbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+char		*treat_cd_path(char *cdpath, char *arg)
+{
+	char		**split_cdpath;
+	struct stat	s;
+	int			i;
+	char		*path;
+
+	i = 0;
+	split_cdpath = ft_strsplit(cdpath, ':');
+	while (split_cdpath && split_cdpath[i])
+	{
+		add_slash(&split_cdpath[i]);
+		split_cdpath[i] = ft_strapp(split_cdpath[i], arg);
+		if (lstat(split_cdpath[i], &s) != -1)
+		{
+			if (S_ISDIR(s.st_mode))
+			{
+				path = ft_strdup(split_cdpath[i]);
+				del_tabstr(&split_cdpath);
+				return (path);
+			}
+		}
+		i++;
+	}
+	del_tabstr(&split_cdpath);
+	return (NULL);
+}
+
+static int	exec_cd_default_cdpath(char **curpath, char ***env, char *arg)
+{
+	char	*pwd;
+
+	if (*curpath && (*curpath)[0] != '/')
+	{
+		if (!(pwd = get_elem(env, "PWD=")))
+			if (get_loc("PWD"))
+				pwd = get_loc("PWD")->value;
+		pwd = ft_strdup(pwd);
+		add_slash(&pwd);
+		pwd = ft_strapp(pwd, *curpath);
+		check_dotdot(&pwd);
+	}
+	else
+		pwd = ft_strdup(*curpath);
+	if (!(*curpath) || chdir(pwd) == -1)
+	{
+		ft_strdel(curpath);
+		return (error_chdir(&pwd, arg, 1));
+	}
+	ft_strdel(curpath);
+	ft_putendl(pwd);
+	change_env(env, pwd);
+	ft_strdel(&pwd);
+	return (0);
+}
+
+static int	exec_cd_p_cdpath(char **curpath, char ***env, char *arg)
+{
+	char	pwd[256];
+
+	if (!(*curpath) || chdir(*curpath) == -1)
+		return (error_chdir(curpath, arg, 1));
+	ft_strdel(curpath);
+	ft_putendl(arg);
+	getcwd(pwd, 256);
+	change_env(env, pwd);
+	return (0);
+}
+
+int			cd_treat_path_cdpath(char **path, char opt, char ***env, char *arg)
+{
+	if (opt == 'P')
+		return (exec_cd_p_cdpath(path, env, arg));
+	else
+		return (exec_cd_default_cdpath(path, env, arg));
+}
---
+++ srcs/built-in/cd/ft_cd.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/26 12:02:15 by lbopp             #+#    #+#             */
+/*   Updated: 2017/10/30 17:25:15 by lbopp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+void	add_slash(char **content)
+{
+	if ((*content)[ft_strlen(*content) - 1] != '/')
+		*content = ft_strapp(*content, "/");
+}
+
+void	check_cdopt(char **path, char *opt)
+{
+	int	i;
+	int	ret;
+
+	i = 0;
+	while (path[i])
+		i++;
+	g_optind = 1;
+	while ((ret = ft_getopt(i, (const char**)path, "LP")) != -1)
+	{
+		*opt = ret;
+		if (*opt == '?')
+			g_optind = -1;
+	}
+}
+
+void	change_env(char ***env, char *pwd)
+{
+	char	*oldpwd;
+	char	**tmp;
+
+	if (!(oldpwd = get_elem(env, "PWD=")) && get_loc("PWD"))
+		oldpwd = get_loc("PWD")->value;
+	oldpwd = ft_strdup(oldpwd);
+	if (get_elem(env, "PWD=") || get_loc("PWD"))
+		add_loc("PWD", pwd);
+	else
+	{
+		tmp = prep_setenv("PWD", pwd);
+		ft_setenv(tmp, env);
+		del_tabstr(&tmp);
+	}
+	if (get_elem(env, "OLDPWD=") || get_loc("OLDPWD"))
+		add_loc("OLDPWD", oldpwd);
+	else
+	{
+		tmp = prep_setenv("OLDPWD", oldpwd);
+		ft_setenv(tmp, env);
+		del_tabstr(&tmp);
+	}
+	ft_strdel(&oldpwd);
+}
+
+int		cd_exec(char ***env, char **tab, char opt)
+{
+	char	*curpath;
+	char	*cdpath;
+
+	curpath = NULL;
+	if (tab[g_optind] && tab[g_optind][0] == '/')
+		return (cd_treat_path(tab[g_optind], opt, env, tab[g_optind]));
+	else if (tab[g_optind] && tab[g_optind][0] == '.')
+		return (cd_treat_path(tab[g_optind], opt, env, tab[g_optind]));
+	else if (tab[g_optind] && ((cdpath = get_elem(env, "CDPATH="))
+			|| (get_loc("CDPATH") && (cdpath = get_loc("CDPATH")->value))))
+	{
+		curpath = treat_cd_path(cdpath, tab[g_optind]);
+		return (cd_treat_path_cdpath(&curpath, opt, env, tab[g_optind]));
+	}
+	else if (tab[g_optind])
+		return (cd_treat_path(tab[g_optind], opt, env, tab[g_optind]));
+	return (1);
+}
+
+int		ft_cd(char **tab, char ***env)
+{
+	char	opt;
+
+	opt = 'L';
+	check_cdopt(tab, &opt);
+	if (g_optind == -1)
+		return (1);
+	if (!tab[g_optind])
+		return (cd_home(env, opt));
+	else if (tab[g_optind] && !ft_strcmp(tab[g_optind], "-"))
+		return (cd_oldpwd(env, opt));
+	else
+		return (cd_exec(env, tab, opt));
+	return (0);
+}
