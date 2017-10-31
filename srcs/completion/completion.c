612073224152b96e83f4372a7897bd520a90f6f5
Arnaud MAZURIER <amazurie@e1r10p23.42.fr>
2017-10-31 15:34:11 +0100

    fix completion space in path

--- srcs/completion/completion.c
+++ srcs/completion/completion.c
@@ -6,24 +6,11 @@
 /*   By: amazurie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 09:54:57 by amazurie          #+#    #+#             */
-/*   Updated: 2017/10/31 13:28:47 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/10/31 15:32:21 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
-
-void		compl_addline(t_cmd *cmd, t_compl *compl)
-{
-	int		i;
-
-	i = ft_strlen(compl->arg);
-	while (i-- && cmd->col > cmd->prlen + 1)
-	{
-		cmd->str = ft_strdelone(cmd->str, (cmd->col - 1) - cmd->prlen);
-		go_left(cmd);
-	}
-	add_line(cmd, compl->args.arg);
-}
 
 static void	compl_issigint(int n)
 {
@@ -91,6 +78,17 @@ static void	init_compl(t_compl *compl, t_cmd *cmd)
 	signal(SIGINT, &compl_issigint);
 }
 
+static void	reslash(char **s)
+{
+	char	*tmp;
+
+	if (!s || !*s)
+		return ;
+	tmp = add_handspace(*s);
+	free(*s);
+	*s = tmp;
+}
+
 void		completion(t_cmd *cmd, char ***env, char **buf)
 {
 	t_compl	compl;
@@ -98,13 +96,14 @@ void		completion(t_cmd *cmd, char ***env, char **buf)
 
 	init_compl(&compl, cmd);
 	list_compl(&compl, cmd, env);
+	reslash(&(compl.arg));
 	if (!compl.args.arg || compl_star(&compl, cmd))
 	{
 		ft_bzero(*buf, ft_strlen(*buf));
 		return (compl_free(&compl));
 	}
 	(!compl.isslash && compl.path && compl.path[0]) ? add_line(cmd, "/") : 0;
-	if (!compl.args.next && compl.args.arg)
+	if ((i = 1) && !compl.args.next && compl.args.arg)
 	{
 		compl_addline(cmd, &compl);
 		ft_bzero(*buf, ft_strlen(*buf));
@@ -113,7 +112,6 @@ void		completion(t_cmd *cmd, char ***env, char **buf)
 		return (compl_free(&compl));
 	}
 	display_args(&compl, cmd);
-	i = 1;
 	while (i > 0)
 		if ((i = compl_keys(&compl, cmd, buf)) == -1)
 			ft_bzero(*buf, ft_strlen(*buf));
