bd280b139fc6e2b175926457c3dc1cb9a79f5ec9
amazurie <amazu@live.fr>
2017-11-04 13:56:27 +0100

    parse world modifier

--- includes/line_editor.h
+++ includes/line_editor.h
@@ -6,7 +6,7 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/13 16:36:09 by hublanc           #+#    #+#             */
-/*   Updated: 2017/11/04 12:58:59 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/11/04 13:35:24 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,10 @@ typedef struct		s_bang2
 	int				m_p;
 	int				m_q;
 	int				m_x;
+	int				m_s;
+	int				m_g;
+	char			*old;
+	char			*new;
 	char			*str;
 }					t_bang2;
 
--- srcs/line_editor/bang/word_modifier.c
+++ srcs/line_editor/bang/word_modifier.c
@@ -6,17 +6,39 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/03 13:40:21 by hublanc           #+#    #+#             */
-/*   Updated: 2017/11/03 20:18:35 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/11/04 13:55:33 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+static void	check_smodif(char *cmd, t_bang2 *bang, int i)
+{
+	char	t;
+	int		j;
+
+	t = cmd[++i];
+	j = 0;
+	while (cmd[i] && (cmd[i] != ' ' || cmd[i - 1] == '\\') && !bang->new)
+	{
+		if (cmd[i] == t && cmd[i - 1] != '\\' && !bang->old)
+			bang->old = ft_strndup(cmd + (i - j), j);
+		else if (cmd[i] == t && cmd[i - 1] != '\\')
+			bang->new = ft_strndup(cmd + (i - j), j);
+		if (cmd[i] == t && cmd[i - 1] != '\\')
+			j = -1;
+		i++;
+		j++;
+	}
+	if (bang->old && !bang->new)
+		bang->new = ft_strndup(cmd + (i - j), j);
+}
+
 int			word_modifier(char *cmd, t_bang2 *bang, int i)
 {
 	if (!cmd || !cmd[i] || cmd[i] != ':')
 		return (i);
-	while (cmd[i] && cmd[i] != ':')
+	while (cmd[i] && (cmd[i] == ':' || bang->m_g == 1))
 	{
 		i++;
 		if (cmd[i] && cmd[i] == 'h')
@@ -33,6 +55,12 @@ int			word_modifier(char *cmd, t_bang2 *bang, int i)
 			bang->m_q = 1;
 		if (cmd[i] && cmd[i] == 'x')
 			bang->m_x = 1;
+		if (cmd[i] && cmd[i] == 's')
+			check_smodif(cmd, bang, i);
+		if (bang->m_g == 1)
+			return (i);
+		if (cmd[i] && cmd[i] == 'g')
+			bang->m_g = 1;
 		cmd[i] ? i++ : 0;
 	}
 	return (i);
