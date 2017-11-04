82295e2f8af6da21ef0438e1c83b3c122a24d66a
Hugo BLANC <hublanc@e1r9p17.42.fr>
2017-11-04 16:13:33 +0100

    fix major

--- srcs/line_editor/bang/word_designator.c
+++ srcs/line_editor/bang/word_designator.c
@@ -6,7 +6,7 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/03 13:29:08 by hublanc           #+#    #+#             */
-/*   Updated: 2017/11/04 14:32:59 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/11/04 16:11:00 by hublanc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,14 @@ int			get_ydigit(char *cmd, t_bang2 *bang, int i)
 
 int			word_designator_y(char *cmd, t_bang2 *bang, int i)
 {
-	if (!cmd || !cmd[i] || cmd[i] != '-' || bang->dash == 1
-		|| bang->shortcut)
+	if (!cmd || !cmd[i] || cmd[i] != '-' || bang->c_y == '*'
+		|| bang->c_y == '$' || bang->shortcut)
 		return (i);
+	i++;
 	bang->dash = 1;
-	if (cmd[i + 1] && cmd[i + 1] == '^' && i++)
-		bang->c_y = '^';
-	else if (cmd[i + 1] && cmd[i + 1] == '$' && i++)
+	if (cmd[i] && cmd[i] == '$' && i++)
 		bang->c_y = '$';
-	else if (cmd[i + 1] && cmd[i + 1] == '*' && i++)
-		bang->c_y = '*';
-	else if (cmd[i + 1] && ft_isdigit(cmd[i + 1]) && i++)
+	else if (cmd[i] && ft_isdigit(cmd[i]))
 		i = get_ydigit(cmd, bang, i);
 	return (i);
 }
@@ -49,8 +46,8 @@ int			get_xdigit(char *cmd, t_bang2 *bang, int i)
 		i++;
 	if (cmd[i] && cmd[i] == '*')
 	{
-		bang->dash = 1;
 		bang->c_y = '*';
+		i++;
 	}
 	bang->x_set = 1;
 	return (i);
@@ -60,15 +57,22 @@ int			word_designator_x(char *cmd, t_bang2 *bang, int i)
 {
 	if (!cmd || !cmd[i] || cmd[i] != ':' || bang->shortcut)
 		return (i);
-	if (cmd[i + 1] && (cmd[i + 1] == '^' || cmd[i + 1] == '$'
-		|| cmd[i + 1] == '*' || ft_isdigit(cmd[i + 1])))
+	if (cmd[i + 1] && (cmd[i + 1] == '^' || cmd[i + 1] == '*'
+		|| cmd[i + 1] == '$' || ft_isdigit(cmd[i + 1])))
 		i++;
 	if (cmd[i] && cmd[i] == '^' && i++)
+	{
+		if (cmd[i] && cmd[i] == '*')
+		{
+			bang->c_y = '*';
+			i++;
+		}
 		bang->c_x = '^';
-	else if (cmd[i] && cmd[i] == '$' && i++)
-		bang->c_x = '$';
+	}
 	else if (cmd[i] && cmd[i] == '*' && i++)
 		bang->c_x = '*';
+	else if (cmd[i] && cmd[i] == '$' && i++)
+		bang->c_y = '$';
 	else if (cmd[i] && ft_isdigit(cmd[i]))
 		i = get_xdigit(cmd, bang, i);
 	else if (cmd[i + 1] == '-')
