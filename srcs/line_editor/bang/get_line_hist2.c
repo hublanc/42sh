6e5fe64dd73fe26e7a61f88be90a2f50c899f30b
amazurie <amazu@live.fr>
2017-11-05 22:57:16 +0100

    !-54435 ou !-0 error msg

--- srcs/line_editor/bang/get_line_hist2.c
+++ srcs/line_editor/bang/get_line_hist2.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/03 17:15:28 by lbopp             #+#    #+#             */
-/*   Updated: 2017/11/05 17:20:10 by lbopp            ###   ########.fr       */
+/*   Updated: 2017/11/05 22:55:54 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,8 @@ static char	**beginsearch_nl(t_control *hist, t_bang2 *bang)
 
 char		**get_nline(t_control *hist, t_bang2 *bang)
 {
+	char	*s;
+
 	if (bang->cmd_l < 0)
 		return (endsearch_nl(hist, bang));
 	else if (bang->cmd_l > 0)
@@ -63,7 +65,11 @@ char		**get_nline(t_control *hist, t_bang2 *bang)
 	}
 	else if (bang->cmd_l == 0 && bang->n_neg && bang->n_set)
 	{
-		ft_putstr_fd("shell: !-: event not found\n", 2);
+		ft_putstr_fd("shell: !-", 2);
+		s = ft_itoa(bang->cmd_l);
+		ft_putstr_fd(s, 2);
+		s ? ft_strdel(&s) : 0;
+		ft_putstr_fd(": event not found\n", 2);
 		return (NULL);
 	}
 	return (NULL);
