9bf11034324dbd8bc88d450a0a567677e98cb86b
Lucas BOPP <lbopp@student.42.fr>
2017-11-05 12:31:49 +0100

    Check quote after bang and not before

--- srcs/exec/routine.c
+++ srcs/exec/routine.c
@@ -6,7 +6,7 @@
 /*   By: hublanc <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 11:10:52 by hublanc           #+#    #+#             */
-/*   Updated: 2017/11/04 23:48:54 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/11/05 12:31:04 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,20 +106,15 @@ void		routine(char *cmd, char ***env, t_control **history)
 {
 	t_token		*list;
 	t_node		*tree;
-	char		*new_command;
 
 	if (!cmd)
 		return ;
-	new_command = deal_bang(cmd, *history);
-	if (new_command == NULL)
+	if (cmd == NULL)
 		return ;
-	list = tokenizer(new_command);
+	list = tokenizer(cmd);
 	list = sort_token(list, history);
 	if (!list)
-	{
-		ft_strdel(&new_command);
 		return ;
-	}
 	tree = create_tree(list);
 	if (tree == NULL)
 		return ;
@@ -128,5 +123,4 @@ void		routine(char *cmd, char ***env, t_control **history)
 	set_terminal();
 	del_token(&list);
 	destroy_tree(tree);
-	ft_strdel(&new_command);
 }
