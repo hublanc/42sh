5218a6e02bf7d3971d4a512ded24d5aa5e6186fb
amazurie <amazu@live.fr>
2017-11-06 03:58:00 +0100

    !0

--- srcs/line_editor/bang/get_line_hist2.c
+++ srcs/line_editor/bang/get_line_hist2.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/03 17:15:28 by lbopp             #+#    #+#             */
-/*   Updated: 2017/11/05 22:55:54 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/11/06 03:57:21 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,26 @@ static char	**beginsearch_nl(t_control *hist, t_bang2 *bang)
 	return (bang_split(tmp->name));
 }
 
+static char	**beginsearch_zer(t_control *hist, t_bang2 *bang)
+{
+	t_lst	*tmp;
+	char	**tab;
+
+	tmp = hist->begin;
+	while (tmp)
+	{
+		tab = bang_split(tmp->name);
+		if (tab && tab[0] && !tab[1])
+			return (tab);
+		tmp = tmp->prev;
+		del_tabstr(&tab);
+	}
+	ft_putstr_fd("shell: !", 2);
+	ft_putnbr_fd(bang->cmd_l, 2);
+	ft_putstr_fd(": event not found\n", 2);
+	return (NULL);
+}
+
 char		**get_nline(t_control *hist, t_bang2 *bang)
 {
 	char	*s;
@@ -59,10 +79,7 @@ char		**get_nline(t_control *hist, t_bang2 *bang)
 	else if (bang->cmd_l > 0)
 		return (beginsearch_nl(hist, bang));
 	else if (bang->cmd_l == 0 && bang->n_set && !bang->n_neg)
-	{
-		ft_putstr_fd("shell: !0: event not found\n", 2);
-		return (NULL);
-	}
+		return (beginsearch_zer(hist, bang));
 	else if (bang->cmd_l == 0 && bang->n_neg && bang->n_set)
 	{
 		ft_putstr_fd("shell: !-", 2);
