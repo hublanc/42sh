32f9e641697980af9b0ba0b2493d85720175bf29
Lucas BOPP <lbopp@e1r12p21.42.fr>
2017-11-10 13:21:01 +0100

    Redir not bad I think but need test. It is a save

--- srcs/redirection/file.c
+++ srcs/redirection/file.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/03 19:10:40 by lbopp             #+#    #+#             */
-/*   Updated: 2017/11/09 16:50:31 by hublanc          ###   ########.fr       */
+/*   Updated: 2017/11/10 13:19:51 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,20 @@ static void		abort_redir(t_node *tree)
 	close_fd(tmp);
 }
 
-static int		authorization_file(char *path, t_node *tree, char *dir,
-				char *link)
+static int		authorization_file(char *path, t_node *tree, char *link)
 {
 	struct stat	s;
 
-	if (ft_strchr(link, '/') && dir && access(dir, F_OK) == -1)
-		return (error_redir(path, "No such file or directory"));
-	else if (ft_strchr(link, '/') && dir && access(dir, X_OK) == -1)
-		return (error_redir(path, "Permission denied"));
-	else if (access(link, F_OK) == -1 && type_redir(tree->token) == 1)
+	ft_putcolor("FILE : ");
+	ft_putendl(link);
+	if (link[ft_strlen(link) - 1] == '/')
+	{
+		if (lstat(link, &s) != -1 && S_ISDIR(s.st_mode) && type_redir(tree->token) == 1)
+			return (error_redir(path, "Is a directory"));
+		else if (access(link, F_OK) == -1)
+			return (error_redir(path, "Not a directory"));
+	}
+	if (access(link, F_OK) == -1 && type_redir(tree->token) == 1)
 		return (0);
 	else if (access(link, F_OK) == -1)
 		return (error_redir(path, "No such file or directory"));
@@ -62,27 +66,52 @@ static int		authorization_file(char *path, t_node *tree, char *dir,
 		return (error_redir(path, "Is a directory"));
 	else if (access(link, F_OK) != -1
 			&& ((access(link, W_OK) == -1 && type_redir(tree->token) == 1)
-			|| (access(link, R_OK) == -1 && type_redir(tree->token) == 2)))
+				|| (access(link, R_OK) == -1 && type_redir(tree->token) == 2)))
 		return (error_redir(path, "Permission denied"));
 	else if (access(link, F_OK) != -1
 			&& ((!access(link, W_OK) && type_redir(tree->token) == 1)
-			|| (!access(link, R_OK) && type_redir(tree->token) == 2)))
+				|| (!access(link, R_OK) && type_redir(tree->token) == 2)))
 		return (0);
 	return (1);
 }
 
+int				check_dir(char *path, char *dir)
+{
+	ft_putcolor("DIR : ");
+	ft_putendl(dir);
+	if (dir && dir[0] && access(dir, F_OK) == -1)
+		return (error_redir(path, "No such file or directory"));
+	else if (dir && dir[0] && access(dir, X_OK) == -1)
+		return (error_redir(path, "Permission denied"));
+	return (0);
+}
+
 static char		*check_file(char *file, t_node *tree)
 {
-	char		*dir;
-	char		link[256];
+	char	*dir;
+	char	link[256];
+	int		i;
+	int		error;
+	char	**split;
 
-	if (ft_strcmp(file, "/"))
-		ft_strtrimlast(&file, '/');
-	dir = ft_strsub(file, 0,
-		ft_strlen(file) - ft_strlen(ft_strrchr(file, '/')));
 	if (readlink(file, link, 256) == -1)
 		ft_strcpy(link, file);
-	if (authorization_file(file, tree, dir, link))
+	split = ft_strsplit(link, '/');
+	i = 0;
+	if (link[0] == '/')
+		dir = ft_strdup("/");
+	else
+		dir = ft_strnew(0);
+	error = check_dir(file, dir);
+	while (split && split[i] && split[i + 1] && !error)
+	{
+		dir = ft_strapp(dir, split[i]);
+		if (link[ft_strlen(dir)] == '/')
+			dir = ft_strapp(dir, "/");
+		error = check_dir(file, dir);
+		i++;
+	}
+	if (error || authorization_file(file, tree, link))
 	{
 		abort_redir(tree);
 		ft_strdel(&file);
@@ -105,10 +134,10 @@ int				open_file(t_node *tree)
 		return (-1);
 	if ((tree->token)[i] == '>' && (tree->token)[i + 1] == '>')
 		fd = open(file,
-		O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+				O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	else if ((tree->token)[i] == '>')
 		fd = open(file,
-		O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+				O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	else if ((tree->token)[i] == '<' && (tree->token)[i + 1] != '<')
 		fd = open(file, O_RDONLY);
 	ft_strdel(&file);
